cdf09afebd7af9a923f8c290cd673ffc82365a3b
Brent Shaffer <betterbrent@google.com>
2020-12-07 15:27:38 -0800

    fix: lower protobuf hash order of magnitude for PHP

--- src/main/java/com/google/cloud/discotoproto3converter/proto3/Message.java
+++ src/main/java/com/google/cloud/discotoproto3converter/proto3/Message.java
@@ -92,16 +92,16 @@ public class Message extends ProtoElement {
   // All the "magic numbers" come from the proto3 spec:
   // https://developers.google.com/protocol-buffers/docs/proto3#assigning_field_numbers
   private int getFieldNumber(String fieldName) {
-    int fieldNumber = (fieldName.hashCode() << 3) >>> 3;
+    int fieldNumber = (fieldName.hashCode() << 4) >>> 4;
     if (fieldNumber == 0 || (fieldNumber >= 19000 && fieldNumber <= 19999)) {
-      fieldNumber = 20000 + ((fieldNumber % 19000) + 1) * 536314;
+      fieldNumber = 20000 + ((fieldNumber % 19000) + 1) * 268147;
     }
     return fieldNumber;
   }
 
   private int incrementFieldNumber(int fieldNumber) {
     int incrementedFieldNumber = fieldNumber + 1;
-    if ((fieldNumber >= 19000 && fieldNumber <= 19999) || fieldNumber >= (1 << 29)) {
+    if ((fieldNumber >= 19000 && fieldNumber <= 19999) || fieldNumber >= (1 << 28)) {
       incrementedFieldNumber = 20000;
     }
     return incrementedFieldNumber;
