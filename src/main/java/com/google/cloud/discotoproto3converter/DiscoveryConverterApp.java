9ef238397903aa3f4a05c28c9eb0127dcf765ee2
Victor Chudnovsky <vchudnov@google.com>
2025-06-18 10:02:47 -0700

    (WIP) Create DiscoveryConverterApp to generate all files together.
    This will allow us to revert DiscoZToProto3ConverterApp

---
+++ src/main/java/com/google/cloud/discotoproto3converter/DiscoveryConverterApp.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.discotoproto3converter;
+
+import com.google.cloud.discotoproto3converter.proto3.Proto3Writer;
+import java.io.IOException;
+import java.util.Map;
+
+public class DiscoveryConverterApp extends ConverterApp {
+  public DiscoveryConverterApp() {
+    super(new Proto3Writer());
+  }
+
+  public static void main(String[] args) throws IOException {
+    Map<String, String> parsedArgs = parseArgs(args);
+    String outputFilePathArgument="--output_file_path";
+    String outputFileStem = parsedArgs.get(outputFilePathArgument);
+
+    System.err.print("\n\n\n*** Generating protocol buffer file");
+    parsedArgs.put(outputFilePathArgument, outputFileStem.concat(".proto"));
+    DiscoToProto3ConverterApp protoConverterApp = new DiscoToProto3ConverterApp();
+    protoConverterApp.convert(parsedArgs);
+
+    System.err.print("\n\n\n*** Generating grpc service config");
+    parsedArgs.put(outputFilePathArgument, outputFileStem.concat("_grpc_service_config.json"));
+    ServiceConfigGeneratorApp serviceConfigConverterApp = new ServiceConfigGeneratorApp();
+    serviceConfigConverterApp.convert(parsedArgs);
+
+    System.err.print("\n\n\n*** Generating gapic yaml config");
+    parsedArgs.put(outputFilePathArgument, outputFileStem.concat("_gapic.yaml"));
+    GapicYamlGeneratorApp gapicYamlGeneratorApp = new GapicYamlGeneratorApp();
+    gapicYamlGeneratorApp.convert(parsedArgs);
+  }
+}
